de38b8ed9e9e79e7a25006bf8615c0c4de8858cf
Aurélien <aurelien.strimelle@bstorm.be>
2026-08-21 15:57:52 +0200

    Network Test : PokemonRequester

--- src/mocks/pokemon.handlers.js
+++ src/mocks/pokemon.handlers.js
@@ -4,9 +4,13 @@ import pokemonData from './../data/pokeapi-result.json';
 const pokemonHandlers = [
 
     http.get('https://pokeapi.co/api/v2/pokemon/100', () => {
+        // Envoi des données du pokemon voltorbe
         return HttpResponse.json(pokemonData)
-    })
+    }),
 
-
+    http.get('https://pokeapi.co/api/v2/pokemon/-42', () => {
+        // Erreur serveur
+        return new HttpResponse(null, { status : 500 });
+    }),
 ];
 export default pokemonHandlers;
